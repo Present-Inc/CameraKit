77bb3f1db8dca63775692ee8aa6ab5ff84ea2244
Justin Makaila <justinmakaila@gmail.com>
2015-04-05 20:06:46 -0400

    Initial Commit

---
+++ CameraKit/CameraKit.h
@@ -0,0 +1,19 @@
+//
+//  CameraKit.h
+//  CameraKit
+//
+//  Created by Justin Makaila on 4/5/15.
+//  Copyright (c) 2015 Present, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraKit.
+FOUNDATION_EXPORT double CameraKitVersionNumber;
+
+//! Project version string for CameraKit.
+FOUNDATION_EXPORT const unsigned char CameraKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraKit/PublicHeader.h>
+
+
